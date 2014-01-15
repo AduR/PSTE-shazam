d0dff056eded4e86ae1ed6a1ddbc4cd1254d7181
Adrien du Ranquet <adrien.duranquet@laposte.net>
2014-01-15 21:10:03 +0100

    make hash for all files in a directory

---
+++ Make_hash/make_hash_all.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+MUSIC_PATH="../../musiques_all"
+PROG_NAME=make_hash
+
+NB=1
+mkdir Hash
+cp includes/constantes.h ./Hash/constantes.h
+for file_in_full in $(find $MUSIC_PATH -iname '*.wav'); 
+do
+	echo ""
+	echo "====="
+	echo $NB
+	file_in_namefull=$(basename "$file_in_full")
+	file_in_nameshort="${file_in_namefull%.*}"
+	file_out_path=hash/$file_in_nameshort.txt
+	echo "$file_in_full > $file_out_path"
+	echo $file_in_full
+	time ./$PROG_NAME $file_in_full > $file_out_path
+	(( NB=NB+1 ))
+done
+
